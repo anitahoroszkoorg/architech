b51dad629bddc8383854fb755c9bdca5db908c78
Anita <ahlistopad@gmail.com>
2022-08-04 19:45:24 +0200

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,14 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
+const icons = [<TwitterIcon />, <FacebookIcon />, <YouTubeIcon />];
+
 export default function LabelBottomNavigation() {
   return (
     <BottomNavigation sx={{ width: "100%" }}>
-      <BottomNavigationAction icon={<TwitterIcon sx={{ color: "black" }} />} />
-      <BottomNavigationAction icon={<FacebookIcon sx={{ color: "black" }} />} />
-      <BottomNavigationAction icon={<YouTubeIcon sx={{ color: "black" }} />} />
+      {icons.map((icon) => (
+        <BottomNavigationAction icon={icon} />
+      ))}
     </BottomNavigation>
   );
 }
