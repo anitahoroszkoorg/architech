f7d0b7f964532514ad5f18cf893142b56b2bd758
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2022-10-11 01:53:12 +0200

    fixed conflicts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,7 @@
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000"
-<<<<<<< HEAD
-    : process.env.REACT_APP_API_URL;
-=======
     : "https://backend-dot-architech-362918.lm.r.appspot.com";
->>>>>>> master
 const imageGrid =
   "https://storage.googleapis.com/architech-static/imagegrid.png";
 const logo = "https://storage.googleapis.com/architech-static/logo.png";
