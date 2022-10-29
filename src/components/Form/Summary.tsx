5291c9a02684da4b83f2c265a9a44b65f1e14e13
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2022-10-29 23:08:05 +0200

    created mobile layout for summary page

--- src/components/Form/Summary.tsx
+++ src/components/Form/Summary.tsx
@@ -1,9 +1,9 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
 import WhiteContainer from "containers/WhiteContainer";
 import Footer from "components/Footer/Footer";
 import Contract from "components/Form/Contract";
 
-function Summary() {
+function DesktopSummary() {
   return (
     <>
       <WhiteContainer isLast>
@@ -21,7 +21,7 @@ function Summary() {
               variant="body1"
               gutterBottom
               component="div"
-              sx={{ color: "black", mt: 2, ml: 2 }}
+              sx={{ color: "primary.main", mt: 2, ml: 2 }}
             >
               If all data is correct, please proceed and we'll begin the
               verification process.
@@ -39,4 +39,57 @@ function Summary() {
   );
 }
 
+function MobileSummary() {
+  return (
+    <>
+      <WhiteContainer isLast>
+        <>
+          <Typography
+            variant="h6"
+            gutterBottom
+            component="div"
+            sx={{ color: "primary.main", marginTop: 2 }}
+          >
+            Please verify all your information in the contract.
+          </Typography>
+          <Typography
+            variant="body1"
+            gutterBottom
+            component="div"
+            sx={{
+              color: "primary.main",
+              fontWeight: "bold",
+              textDecoration: "underline",
+            }}
+          >
+            Click here to download the PDF file of your contract.
+          </Typography>
+          <Typography
+            variant="body1"
+            gutterBottom
+            component="div"
+            sx={{ color: "primary.main", mt: 2 }}
+          >
+            If all data is correct, please proceed and we'll begin the
+            verification process.
+          </Typography>
+          <Grid container justifyContent="center">
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <Grid container justifyContent="center"></Grid>
+            </Grid>
+          </Grid>
+        </>
+      </WhiteContainer>
+      <Grid item xs={12}>
+        <Footer />
+      </Grid>
+    </>
+  );
+}
+function Summary() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
+
+  return matches ? <DesktopSummary /> : <MobileSummary />;
+}
 export default Summary;
